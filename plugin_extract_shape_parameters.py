db98e1f8a4fef21c87833aa14eafd31cc4058664
Rutger Kok <rutger.kok@live.com>
2023-03-02 11:26:55 +0100

    Allow shape analysis for all open experiments

--- plugin_extract_shape_parameters.py
+++ plugin_extract_shape_parameters.py
@@ -1,17 +1,20 @@
 import math
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 import numpy
+import scipy.ndimage
 import skimage.measure
+import skimage.morphology
 
 from organoid_tracker.core import TimePoint
 from organoid_tracker.core.experiment import Experiment
+from organoid_tracker.core.image_loader import ImageChannel
 from organoid_tracker.core.images import Image
 from organoid_tracker.core.position import Position
 from organoid_tracker.core.position_data import PositionData
 from organoid_tracker.core.resolution import ImageResolution
 from organoid_tracker.gui import dialog, action
-from organoid_tracker.gui.gui_experiment import GuiExperiment
+from organoid_tracker.gui.gui_experiment import GuiExperiment, SingleGuiTab
 from organoid_tracker.gui.threading import Task
 from organoid_tracker.gui.window import Window
 
@@ -23,49 +26,44 @@ def get_menu_items(window: Window):
 
 
 def _extract_segmentation_parameters(window: Window):
-    experiment_gui = window.get_gui_experiment()
-    resolution = experiment_gui.get_experiment().images.resolution()
-    temporary_experiment = experiment_gui.get_experiment().copy_selected(positions=True, links=True, connections=True)
-
-    if not dialog.popup_message_cancellable("Incorporate segmentation", f"Please select the segmentation for this organoid."):
+    if not dialog.popup_message_cancellable("Incorporate segmentation", f"Please make sure that you have the channel"
+        f" with the segmentation masks selected (so not the nuclear fluorescence)."):
         return
-
-    # Load images
-    from organoid_tracker.gui import image_series_loader_dialog
-    if not image_series_loader_dialog.prompt_image_series(temporary_experiment):
-        return
-    # Copy over resolution and offsets
-    temporary_experiment.images.set_resolution(resolution)
-    temporary_experiment.images.offsets = experiment_gui.get_experiment().images.offsets
 
     # Start!
-    window.get_scheduler().add_task(_AnalyzeShapesTask(experiment_gui, temporary_experiment))
+    open_tabs = window.get_gui_experiment().get_active_tabs()
+    window.get_scheduler().add_task(_AnalyzeShapesTask(open_tabs, window.display_settings.image_channel))
 
 
 class _AnalyzeShapesTask(Task):
 
-    _experiment_original: GuiExperiment
-    _experiment_copy: Experiment
+    _open_tabs: List[SingleGuiTab]
+    _experiment_copies: List[Experiment]
+    _segmentation_channel: ImageChannel
 
-    def __init__(self, experiment_original: GuiExperiment, experiment_copy: Experiment):
-        self._experiment_original = experiment_original
-        self._experiment_copy = experiment_copy
+    def __init__(self, open_tabs: List[SingleGuiTab], segmentation_channel: ImageChannel):
+        self._open_tabs = open_tabs
+        self._experiment_copies = [open_tab.experiment.copy_selected(positions=True, links=True, connections=True, images=True)
+                                   for open_tab in open_tabs]
+        self._segmentation_channel = segmentation_channel
 
-    def compute(self) -> PositionData:
-        return _analyze_shapes(self._experiment_copy)
+    def compute(self) -> List[PositionData]:
+        return [_analyze_shapes(experiment_copy, self._segmentation_channel) for experiment_copy in self._experiment_copies]
 
-    def on_finished(self, result: PositionData):
-        position_data = self._experiment_original.get_experiment().position_data
-        for data_name in ["volume_um3", "solidity", "surface_um2", "surface_to_volume_ratio", "label",
-                          "neighbor_distance_avg_um", "neighbor_separation_avg_um"]:
-            position_data.delete_data_with_name(data_name)
+    def on_finished(self, results: List[PositionData]):
+        for open_tab, result in zip(self._open_tabs, results):
+            position_data = open_tab.experiment.position_data
+            # Remove old data
+            for data_name in ["volume_um3", "hole_volume_um3", "solidity", "surface_um2", "surface_to_volume_ratio",
+                              "label", "neighbor_distance_median_um", "neighbor_distance_variation"]:
+                position_data.delete_data_with_name(data_name)
 
-        self._experiment_original.get_experiment().position_data.merge_data(result)
-        self._experiment_original.undo_redo.clear()
+            open_tab.experiment.position_data.merge_data(result)
+            open_tab.undo_redo.clear()
         dialog.popup_message("Extraction finished", "Stored all the metadata of the positions.")
 
 
-def _analyze_shapes(experiment: Experiment) -> PositionData:
+def _analyze_shapes(experiment: Experiment, segmentation_channel: ImageChannel) -> PositionData:
     """Measures on the experiment. The experiment must have only one channel, which contains the segmentation."""
     resolution = experiment.images.resolution()
     results = PositionData()
@@ -73,7 +71,7 @@ def _analyze_shapes(experiment: Experiment) -> PositionData:
     import skimage.measure
     for time_point in experiment.images.time_points():
         print(f"Working on time point {time_point.time_point_number()}...")
-        segmented_image = experiment.images.get_image(time_point)
+        segmented_image = experiment.images.get_image(time_point, segmentation_channel)
         if segmented_image is None:
             continue
 
@@ -89,46 +87,41 @@ def _analyze_shapes(experiment: Experiment) -> PositionData:
         _measure_shape(positions_by_label, regionprops_by_label, resolution, results)
 
         # Second pass: characterize the environment
-        _measure_neighborhood(experiment, positions_by_label, resolution, results)
+        _measure_neighborhood(positions_by_label, regionprops_by_label, resolution, results)
 
     return results
 
 
-def _measure_neighborhood(experiment: Experiment, positions_by_label: Dict[int, Position], resolution: ImageResolution,
-                          results: PositionData):
+def _measure_neighborhood(positions_by_label: Dict[int, Position],
+                          properties_by_label: Dict[int, "skimage.measure._regionprops.RegionProperties"],
+                          resolution: ImageResolution, results: PositionData):
     """Looks at the distances to nearby cells."""
+    resolution_zyx_um = numpy.array(resolution.pixel_size_zyx_um, dtype=numpy.float32)
+
+    # Construct table of *all* positions (including of regions that were segmented, but that we did not track)
+    other_positions_table = numpy.empty((len(properties_by_label), 3), dtype=numpy.float32)
+    for i, properties in enumerate(properties_by_label.values()):
+        other_positions_table[i] = properties.centroid * resolution_zyx_um
+
     for position in positions_by_label.values():
         if position.is_zero():
             continue
 
-        volume_um3 = results.get_position_data(position, "volume_um3")
-        if volume_um3 is None:
-            continue
-        spherical_radius_um = (volume_um3 / (math.pi * 4 / 3)) ** (1 / 3)
+        position_zyx_um = numpy.array([position.z, position.y, position.x], dtype=numpy.float32) * resolution_zyx_um
+        distance_matrix = other_positions_table - position_zyx_um
+        distance_matrix = numpy.sum(distance_matrix ** 2, axis=1)
+        closest_distances_um = distance_matrix[numpy.argpartition(distance_matrix, 8)[2:8]] ** 0.5
+        # We ignore the clostest distance, since that is the position itself
+        # So closest distance 1:7 should represent the distance to the neighbors
+        neighbor_distance_median_um = float(numpy.median(closest_distances_um))
+        neighbor_distance_min_um = float(numpy.min(closest_distances_um))
 
-        neighbor_count = 0
-        neighbor_distance_sum = 0
-        neighbor_separation_sum = 0
-        for neighbor in experiment.connections.find_connections(position):
-            neighbor_volume_um3 = results.get_position_data(neighbor, "volume_um3")
-            if neighbor_volume_um3 is None:
-                continue
-            neighbor_spherical_radius_um = (volume_um3 / (math.pi * 4 / 3)) ** (1 / 3)
-
-            center_to_center_distance = neighbor.distance_um(position, resolution)
-            neighbor_count += 1
-            neighbor_distance_sum += center_to_center_distance
-            neighbor_separation_sum += center_to_center_distance - spherical_radius_um - neighbor_spherical_radius_um
-        if neighbor_count < 3:
-            continue  # Too few neighbors, likely at edge
-
-        results.set_position_data(position, "neighbor_distance_avg_um",
-                                  neighbor_distance_sum / neighbor_count)
-        results.set_position_data(position, "neighbor_separation_avg_um",
-                                  neighbor_separation_sum / neighbor_count)
+        results.set_position_data(position, "neighbor_distance_median_um", neighbor_distance_median_um)
+        results.set_position_data(position, "neighbor_distance_min_um", neighbor_distance_min_um)
 
 
-def _measure_shape(positions_by_label: Dict[int, Position], properties_by_label: Dict[int, Any],
+def _measure_shape(positions_by_label: Dict[int, Position],
+                   properties_by_label: Dict[int, "skimage.measure._regionprops.RegionProperties"],
                    resolution: ImageResolution, results: PositionData):
     """Looks at the shape of a cell."""
     pixel_volume_um3 = resolution.pixel_size_x_um * resolution.pixel_size_y_um * resolution.pixel_size_z_um
@@ -139,21 +132,26 @@ def _measure_shape(positions_by_label: Dict[int, Position], properties_by_label:
             # Segmentation failed for this label
             continue
 
-        volume_um3 = float(properties.area * pixel_volume_um3)
-        solidity = float(properties.solidity)
+        padded = numpy.pad(properties.image_filled, 2, mode='constant', constant_values=0)
+        padded = scipy.ndimage.binary_opening(padded, structure = numpy.ones((3, 5, 5)))
+        volume_um3 = float(numpy.sum(padded) * pixel_volume_um3)
+        if volume_um3 == 0:
+            continue  # Nothing was left after opening
 
-        padded = numpy.pad(properties.image, 2, mode='constant', constant_values=0)
+        convex_hull_image = skimage.morphology.convex_hull_image(padded)
+        solidity = float(numpy.sum(padded) / numpy.sum(convex_hull_image))
+
         vertices, faces, _, _ = skimage.measure.marching_cubes(padded, level=.5,
                                                                spacing=(
                                                                resolution.pixel_size_z_um, resolution.pixel_size_y_um,
                                                                resolution.pixel_size_x_um))
         surface_um2 = float(skimage.measure.mesh_surface_area(vertices, faces))
 
-        surface_to_volume_ratio = surface_um2 / volume_um3
+        sphericity = math.pi ** (1/3) * (6 * volume_um3) ** (2/3) / surface_um2
         results.set_position_data(position, "volume_um3", volume_um3)
         results.set_position_data(position, "solidity", solidity)
         results.set_position_data(position, "surface_um2", surface_um2)
-        results.set_position_data(position, "surface_to_volume_ratio", surface_to_volume_ratio)
+        results.set_position_data(position, "sphericity", sphericity)
         results.set_position_data(position, "label", int(label))
 
 
@@ -167,7 +165,6 @@ def _get_positions_by_label(experiment: Experiment, time_point: TimePoint, segme
         if label in positions_by_label:
             # Two positions in one label, segmentation failed
             # Mark this label as failed. (Note: setting it to None wouldn't work, as then if we add a
-            print("Multiple positions for label " + str(label))
             positions_by_label[label] = Position(0, 0, 0, time_point=time_point)
             continue
         positions_by_label[label] = position
