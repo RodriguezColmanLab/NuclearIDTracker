203cc94c734553ee6a8988e4af0ddb270d6a3c34
Rutger Kok <rutger.kok@live.com>
2023-04-07 09:50:30 +0200

    Change parameter extraction and menu location

--- plugin_extract_shape_parameters.py
+++ plugin_extract_shape_parameters.py
@@ -5,6 +5,7 @@ import numpy
 import scipy.ndimage
 import skimage.measure
 import skimage.morphology
+import skimage.segmentation
 
 from organoid_tracker.core import TimePoint
 from organoid_tracker.core.experiment import Experiment
@@ -25,14 +26,19 @@ _EXTRACTED_PARAMETERS = [
     "solidity",
     "surface_um2",
     "neighbor_distance_mean_um",
-    "neighbor_distance_variation"
+    "neighbor_distance_variation",
+    "feret_diameter_max_um",
+    "intensity_factor",
+    "intensity_factor_local",
+    "ellipticity",
+    "organoid_relative_z_um"
 ]
 _AVERAGING_H = 2
 
 
 def get_menu_items(window: Window):
     return {
-        "Segmentation//Segment-Extract shape parameters...": lambda: _extract_segmentation_parameters(window)
+        "Tools//Process-Segmentation//Extract shape parameters...": lambda: _extract_segmentation_parameters(window)
     }
 
 
@@ -66,8 +72,7 @@ class _AnalyzeShapesTask(Task):
         for open_tab, result in zip(self._open_tabs, results):
             position_data = open_tab.experiment.position_data
             # Remove old data
-            for data_name in ["volume_um3", "hole_volume_um3", "solidity", "surface_um2", "surface_to_volume_ratio",
-                              "label", "sphericity", "neighbor_distance_median_um", "neighbor_distance_mad_um"]:
+            for data_name in _EXTRACTED_PARAMETERS:
                 position_data.delete_data_with_name(data_name)
 
             open_tab.experiment.position_data.merge_data(result)
@@ -76,7 +81,7 @@ class _AnalyzeShapesTask(Task):
 
 
 def _analyze_shapes(experiment: Experiment, segmentation_channel: ImageChannel) -> PositionData:
-    """Measures on the experiment. The experiment must have only one channel, which contains the segmentation."""
+    """Measures on the experiment."""
     resolution = experiment.images.resolution()
     results = PositionData()
 
@@ -84,16 +89,28 @@ def _analyze_shapes(experiment: Experiment, segmentation_channel: ImageChannel) 
     for time_point in experiment.images.time_points():
         print(f"Working on time point {time_point.time_point_number()}...")
         segmented_image = experiment.images.get_image(time_point, segmentation_channel)
-        if segmented_image is None:
+        nuclei_image = experiment.images.get_image(time_point)  # TODO allow selection of nucleus channel
+        if segmented_image is None or nuclei_image is None:
             continue
 
         # Index the properties
         regionprops_by_label = dict()
-        for properties in skimage.measure.regionprops(segmented_image.array):
+        image_size_z, image_size_y, image_size_x = segmented_image.array.shape
+        for properties in skimage.measure.regionprops(segmented_image.array, intensity_image=nuclei_image.array):
             regionprops_by_label[properties.label] = properties
 
         # Index positions to label
         positions_by_label = _get_positions_by_label(experiment, time_point, segmented_image)
+
+        # Remove positions bordering the image
+        for label, properties in regionprops_by_label.items():
+            if label not in positions_by_label:
+                continue  # Already didn't have a position
+            slice_z, slice_y, slice_x = properties.slice
+            if slice_z.start == 0 or slice_z.stop >= image_size_z \
+                    or slice_y.start == 0 or slice_y.stop >= image_size_y \
+                    or slice_x.start == 0 or slice_x.stop >= image_size_x:
+                del positions_by_label[label]
 
         # First pass: characterize the shape of the nuclei
         _measure_shape(positions_by_label, regionprops_by_label, resolution, results)
@@ -140,35 +157,47 @@ def _measure_neighborhood(positions_by_label: Dict[int, Position],
     resolution_zyx_um = numpy.array(resolution.pixel_size_zyx_um, dtype=numpy.float32)
 
     # Construct table of *all* positions (including of regions that were segmented, but that we did not track)
-    other_positions_table = numpy.empty((len(properties_by_label), 3), dtype=numpy.float32)
+    other_positions_table = numpy.empty((len(properties_by_label), 4), dtype=numpy.float32)
     for i, properties in enumerate(properties_by_label.values()):
-        other_positions_table[i] = properties.centroid * resolution_zyx_um
+        other_positions_table[i, 0:3] = properties.centroid * resolution_zyx_um
+        other_positions_table[i, 3] = properties.intensity_mean
 
     for position in positions_by_label.values():
         if position.is_zero():
             continue
 
         position_zyx_um = numpy.array([position.z, position.y, position.x], dtype=numpy.float32) * resolution_zyx_um
-        distance_matrix = other_positions_table - position_zyx_um
+        distance_matrix = other_positions_table[:, 0:3] - position_zyx_um
         distance_matrix = numpy.sum(distance_matrix ** 2, axis=1)
 
         # Find the nearby positions
-        closest_distances_um = distance_matrix[numpy.argpartition(distance_matrix, 7)[0:7]] ** 0.5
+        closest_positions_indices = numpy.argpartition(distance_matrix, 7)[0:7]
+        closest_distances_intensities = other_positions_table[closest_positions_indices, 3]
+        closest_distances_um = distance_matrix[closest_positions_indices] ** 0.5
 
         # We ignore the closest distance, since that is the position itself
-        closest_distances_um = numpy.delete(closest_distances_um, numpy.argmin(closest_distances_um))
+        closest_index = numpy.argmin(closest_distances_um)
+        own_intensity = float(closest_distances_intensities[closest_index])
+        closest_distances_um = numpy.delete(closest_distances_um, closest_index)
+        nearby_intensities = float(numpy.mean(numpy.delete(closest_distances_intensities, closest_index)))
 
         neighbor_distance_median_um = float(numpy.median(closest_distances_um))
         neighbor_distance_mad_um = float(numpy.median(numpy.abs(closest_distances_um - neighbor_distance_median_um)))
         results.set_position_data(position, "neighbor_distance_median_um", neighbor_distance_median_um)
-        results.set_position_data(position, "neighbor_distance_variation", neighbor_distance_mad_um / neighbor_distance_median_um)
+        results.set_position_data(position, "neighbor_distance_variation",
+                                  neighbor_distance_mad_um / neighbor_distance_median_um)
+        results.set_position_data(position, "intensity_factor_local", own_intensity / nearby_intensities)
 
 
 def _measure_shape(positions_by_label: Dict[int, Position],
                    properties_by_label: Dict[int, "skimage.measure._regionprops.RegionProperties"],
                    resolution: ImageResolution, results: PositionData):
     """Looks at the shape of a cell."""
+    if len(positions_by_label) == 0:
+        return
     pixel_volume_um3 = resolution.pixel_size_x_um * resolution.pixel_size_y_um * resolution.pixel_size_z_um
+    median_intensity = numpy.median([properties.intensity_mean for properties in properties_by_label.values()])
+    lowest_z = min(position.z for position in positions_by_label.values())
 
     for label, properties in properties_by_label.items():
         position = positions_by_label.get(label)
@@ -191,10 +220,19 @@ def _measure_shape(positions_by_label: Dict[int, Position],
                                                                    resolution.pixel_size_y_um,
                                                                    resolution.pixel_size_x_um))
         surface_um2 = float(skimage.measure.mesh_surface_area(vertices, faces))
+        distances = scipy.spatial.distance.pdist(vertices, 'sqeuclidean')
+        feret_diameter_max_um = math.sqrt(numpy.max(distances))
+        intensity_factor = float(properties.intensity_mean / median_intensity)
+        ellipticity = float((properties.axis_major_length - properties.axis_minor_length) / properties.axis_major_length)
+        organoid_relative_z_um = (position.z - lowest_z) * resolution.pixel_size_z_um
 
         results.set_position_data(position, "volume_um3", volume_um3)
         results.set_position_data(position, "solidity", solidity)
         results.set_position_data(position, "surface_um2", surface_um2)
+        results.set_position_data(position, "feret_diameter_max_um", feret_diameter_max_um)
+        results.set_position_data(position, "intensity_factor", intensity_factor)
+        results.set_position_data(position, "ellipticity", ellipticity)
+        results.set_position_data(position, "organoid_relative_z_um", organoid_relative_z_um)
 
 
 def _get_positions_by_label(experiment: Experiment, time_point: TimePoint, segmented_image: Image):
