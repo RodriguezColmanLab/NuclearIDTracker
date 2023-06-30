70ea0b916e3bced90300c8fd680bb4dc08612994
Rutger Kok <rutger.kok@live.com>
2023-06-30 08:51:31 +0200

    Some code for creating the cell type transition matrix

--- OrganoidTracker plugins/plugin_calculate_cell_type_transition_matrix.py
+++ OrganoidTracker plugins/plugin_calculate_cell_type_transition_matrix.py
@@ -1,4 +1,7 @@
 """Calculates the cell type transitin matrix for the cell types stem, enterocyte, Paneth, other secretory."""
+from collections import defaultdict
+from typing import Dict, Tuple
+
 from organoid_tracker.core import UserError
 from organoid_tracker.gui.window import Window
 
@@ -9,5 +12,19 @@ def get_menu_items(window: Window):
     }
 
 
+class _TransitionMatrix:
+
+    _transition_matrix: Dict[Tuple[str, str], int]
+
+    def __init__(self):
+        self._transition_matrix = defaultdict(lambda: 0)
+
+    def record_transition(self, from_type: str, to_type: str):
+        self._transition_matrix[(from_type, to_type)] += 1
+
+
 def _calculate_transition_matrix(window: Window):
-    raise UserError("Not yet implemented", "This function is not yet available.")
+    transition_matrix = _TransitionMatrix()
+    for experiment in window.get_active_experiments():
+        for track in experiment.links.find_ending_tracks():
+            ...
