7b4da08f85c18f052cc603cc28663318bf8b9018
Rutger Kok <rutger.kok@live.com>
2023-08-31 10:18:08 +0200

    More lax prediction criteria
    So that more cells get a prediction

--- pipeline_predict_on_organoidtracker_data.py
+++ pipeline_predict_on_organoidtracker_data.py
@@ -9,12 +9,13 @@ from organoid_tracker.imaging import list_io, io
 from organoid_tracker.position_analysis import position_markers
 
 _MODEL_FOLDER = r"../Data/Models/epochs-1-neurons-0"
-_INPUT_FILE = "../Data/Testing data - input - treatments.autlist"
-_OUTPUT_FOLDER = "../Data/Testing data - output - treatments"
+_INPUT_FILE = "../Data/Testing data - input - CD24 and Lgr5.autlist"
+_OUTPUT_FOLDER = "../Data/Testing data - output - CD24 and Lgr5"
 
 # For filtering out missegmentations
-_MIN_VOLUME_UM3 = 60
-_MAX_VOLUME_UM3 = 300
+_MIN_VOLUME_UM3 = 40
+_MAX_VOLUME_UM3 = 1000
+
 
 def predict_organoid(experiment: Experiment):
     # Delete existing cell types
@@ -49,10 +50,10 @@ def predict_organoid(experiment: Experiment):
     # Store the predicted cell types
     experiment.global_data.set_data("ct_probabilities", cell_types)
     for position, probability_by_cell_type in zip(positions_corresponding_to_data_array, probabilities):
-        experiment.position_data.set_position_data(position, "ct_probabilities", [float(prob) for prob in probability_by_cell_type])
+        experiment.position_data.set_position_data(position, "ct_probabilities",
+                                                   [float(prob) for prob in probability_by_cell_type])
         cell_type = cell_types[numpy.argmax(probability_by_cell_type)]
         position_markers.set_position_type(experiment.position_data, position, cell_type)
-
 
 
 def main():
@@ -60,7 +61,8 @@ def main():
     for experiment in list_io.load_experiment_list_file(_INPUT_FILE):
         print(f"Working on {experiment.name}...")
         predict_organoid(experiment)
-        io.save_data_to_json(experiment, os.path.join(_OUTPUT_FOLDER, f"{experiment.name.get_save_name()}.{io.FILE_EXTENSION}"))
+        io.save_data_to_json(experiment,
+                             os.path.join(_OUTPUT_FOLDER, f"{experiment.name.get_save_name()}.{io.FILE_EXTENSION}"))
 
 
 if __name__ == "__main__":
