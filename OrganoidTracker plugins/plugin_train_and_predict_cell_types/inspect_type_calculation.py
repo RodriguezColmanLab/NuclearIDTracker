5ac014139e2c8fe1287f707e074cba62533b65fb
Rutger Kok <rutger.kok@live.com>
2024-01-23 17:17:41 +0100

    Allow disabling calculation parameters

--- OrganoidTracker plugins/plugin_train_and_predict_cell_types/inspect_type_calculation.py
+++ OrganoidTracker plugins/plugin_train_and_predict_cell_types/inspect_type_calculation.py
@@ -1,5 +1,5 @@
 import pickle
-from typing import Optional, NamedTuple, List
+from typing import Optional, NamedTuple, List, Set
 
 import numpy
 import scipy
@@ -18,11 +18,11 @@ from organoid_tracker.visualizer.exitable_image_visualizer import ExitableImageV
 
 
 class _ModelParameters(NamedTuple):
-    """Not the original class, but uses Python's duck typing so that we know which fields exist."""
     cell_type_mapping: List[str]
     input_mapping: List[str]
     scaler: StandardScaler
     regressor: LogisticRegression
+    disabled_parameters: Set[str]
 
 
 def view_cell_type_parameters(window: Window):
@@ -34,7 +34,8 @@ def view_cell_type_parameters(window: Window):
         scaler: StandardScaler = pickle.load(handle)
         regressor: LogisticRegression = pickle.load(handle)
     model_parameters = _ModelParameters(cell_type_mapping=input_output["cell_type_mapping"],
-                                        input_mapping=input_output["input_mapping"], scaler=scaler, regressor=regressor)
+                                        input_mapping=input_output["input_mapping"], scaler=scaler, regressor=regressor,
+                                        disabled_parameters=set())
     activate(_CellTypeParameterViewer(window, model_parameters))
 
 
@@ -60,6 +61,14 @@ class _CellParametersPopup(RichTextPopup):
                    "each cell type using the formula below. Then the chance of each cell type is `score / sum of " \
                    "scores`.\n\n"\
                 + self._view_position_parameters()
+        if url.startswith("disable_parameter/"):
+            feature_name = url[len("disable_parameter/"):]
+            self._parameters.disabled_parameters.add(feature_name)
+            return self.navigate(self.INDEX)
+        if url.startswith("reenable_parameter/"):
+            feature_name = url[len("reenable_parameter/"):]
+            self._parameters.disabled_parameters.remove(feature_name)
+            return self.navigate(self.INDEX)
         return None
 
     def _view_position_parameters(self) -> str:
@@ -68,6 +77,8 @@ class _CellParametersPopup(RichTextPopup):
             return "**We're missing some measurement values, so we cannot calculate the cell type scores.**"
         scaled_values = self._parameters.scaler.transform(values.reshape(1, -1)).flatten()
         numpy.clip(scaled_values, -3, 3, out=scaled_values)
+
+        cell_type_scores = list()
 
         output = ""
         for cell_type, coefficients, intercept in zip(self._parameters.cell_type_mapping,
@@ -81,6 +92,8 @@ class _CellParametersPopup(RichTextPopup):
                                                                  scaled_values):
                 color = ""
                 tag = "span"
+                disable_parameter_link = f" <a href=\"disable_parameter/{parameter_name}\">Disable</a>"
+
                 if abs(coefficient * scaled_value) > 0.5:
                     if coefficient * scaled_value > 0.5:
                         color = " style='color:#2ecc71'"  # Green
@@ -88,13 +101,33 @@ class _CellParametersPopup(RichTextPopup):
                         color = " style='color:#8e44ad'"  # Purple
                     if parameter_name == max_parameter_name or parameter_name == min_parameter_name:
                         tag = "b"  # Most important contributor, make bold
+
+                if parameter_name in self._parameters.disabled_parameters:
+                    # Disable this feature
+                    color = " style='color:#dddddd'"
+                    tag = "s"
+                    disable_parameter_link = f" <a href=\"reenable_parameter/{parameter_name}\">Re-enable</a>"
+
                 statement = "Lower value is better" if coefficient < 0 else "Higher value is better"
-                output += f"<{tag}{color}>{coefficient:.4f} * {parameter_name} [{scaled_value:.4f}]</{tag}> + <small style='color:#dddddd'><i>{statement}</i></small>  \n"
-                logit_score += coefficient * scaled_value
+                output += f"<{tag}{color}>{coefficient:.4f} * {parameter_name} [{scaled_value:.4f}]</{tag}> + <small style='color:#dddddd'><i>{statement}{disable_parameter_link}</i></small>  \n"
+
+                if parameter_name not in self._parameters.disabled_parameters:
+                    logit_score += coefficient * scaled_value
             logit_score += intercept
             output += f"{intercept:.4f} = {logit_score:.4f}\n\n"
             output += f"score = expit(logit_score) = {scipy.special.expit(logit_score):.4f}\n\n"
-        return output
+            cell_type_scores.append(scipy.special.expit(logit_score))
+
+        best_cell_type = self._parameters.cell_type_mapping[numpy.argmax(cell_type_scores)]
+
+
+        preface = f"## Most likely cell type: {best_cell_type}\n\n"
+        if self._parameters.disabled_parameters:
+            preface += f"Disabled parameters: <code>{', '.join(self._parameters.disabled_parameters)}</code>\n\n"
+        else:
+            preface += "All parameters are used in this calculation.\n\n"
+
+        return preface + output
 
 
 class _CellTypeParameterViewer(ExitableImageVisualizer):
