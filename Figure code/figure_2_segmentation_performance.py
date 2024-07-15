5b4618f23187c51403bd5be415bf0ed6b1a3decc
Rutger Kok <rutger.kok@live.com>
2024-07-15 10:18:03 +0200

    Update paths to new location

--- Figure code/figure_2_segmentation_performance.py
+++ Figure code/figure_2_segmentation_performance.py
@@ -11,8 +11,8 @@ from numpy import ndarray
 
 import lib_figures
 
-_GROUND_TRUTH_FOLDER = r"P:\Rodriguez_Colman\vidi_rodriguez_colman\rkok\data_analysis\2023-05 RK0029 Rutger Measuring CellPose performance\Manual segmentation"
-_AUTOMATIC_FOLDER = r"P:\Rodriguez_Colman\vidi_rodriguez_colman\rkok\data_analysis\2023-05 RK0029 Rutger Measuring CellPose performance\CellPose segmentation"
+_GROUND_TRUTH_FOLDER = r"P:\Rodriguez_Colman\vidi_rodriguez_colman\rkok\data_analysis\2023\2023-05 RK0029 Rutger Measuring CellPose performance\Manual segmentation"
+_AUTOMATIC_FOLDER = r"P:\Rodriguez_Colman\vidi_rodriguez_colman\rkok\data_analysis\2023\2023-05 RK0029 Rutger Measuring CellPose performance\CellPose segmentation"
 
 
 class _GroundTruthImage:
